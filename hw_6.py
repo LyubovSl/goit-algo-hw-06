2732cf0c2248e9d4cad1670640fe62b927a800c7
LyubovSL <lyubov.sliva1@gmail.com>
2025-03-08 22:13:30 +0200

    created AddressBook

---
+++ hw_6.py
@@ -0,0 +1,91 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    # реалізація класу
+	def __init__(self, value):
+         if not value.strip():
+            raise ValueError("Name cannot be empty.")  
+         super().__init__(value) # type: ignore
+
+class Phone(Field):
+	def __init__(self, value):
+         if not value.isdigit() or len(value) != 10:
+            raise ValueError("Phone number must be exactly 10 digits.")  
+         super().__init__(value) # type: ignore
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = [] 
+    # реалізація класу
+
+    def add_phone(self, phone_number):
+        self.phones.append(Phone(phone_number))
+
+    def remove_phone(self, phone_number):
+        self.phones.remove(phone_number)   
+
+    def edit_phone(self, old_number, new_number):
+        for phone in self.phones:    
+            if phone.value == old_number:
+                phone.value = new_number
+                return new_number
+            else:
+                raise ValueError("Old phone number not found.")
+    
+
+    def find_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                return phone     
+            else:
+                return None                          
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    
+    def add_record(self, record):
+        self.data[record.name.value] = record   
+    
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+    
+    def __str__(self):
+        return '\n'.join(str(record) for record in self.data.values())
+
+book = AddressBook()
+
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+book.add_record(john_record)
+
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+print(book)
+
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+print(john)
+
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")
+
+book.delete("Jane")
+print(book)
+
